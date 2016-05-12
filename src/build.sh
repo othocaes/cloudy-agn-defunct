70b97d6b6c47272a579a1007988e80060643db5e
othocaeS <xaw5719@wmich.edu>
2016-05-12 17:13:40 -0400

    minor build script tweak.

--- src/build.sh
+++ src/build.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 mkdir -p ../bin/
-#g++ -c ${1}.cpp `libpng-config --cflags` -I shapelib-1.3.0/ -I png++-0.2.5/
-g++ -c ${1}.cpp `libpng-config --cflags` -I png++-0.2.5/
-g++ -o ../bin/${1} ${1}.o `libpng-config --ldflags` 
+source_name=`echo $1|sed 's/\.cpp$//'`
+#g++ -c ${source_name}.cpp `libpng-config --cflags` -I shapelib-1.3.0/ -I png++-0.2.5/
+g++ -c ${source_name}.cpp `libpng-config --cflags` -I png++-0.2.5/
+g++ -o ../bin/${source_name} ${source_name}.o `libpng-config --ldflags` 
 
