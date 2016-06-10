7f4bc149e3601229536168f75ae2ee29b9f7fe4b
caes <xaw5719@wmich.edu>
2016-06-10 02:28:20 -0400

    combined fortfile creation and interpolation fix into meta script

--- scripts/meta/bulk_interpolation_fix.sh
+++ scripts/meta/bulk_interpolation_fix.sh
@@ -3,7 +3,7 @@
 script_dir=$( cd $(dirname $0) ; pwd -P |sed 's@^\(.*\)/scripts.*@\1/scripts@')
 basedir=`pwd`
 
-if [[ -x $script_dir/../bin/interpolate_fix]];
+if [[ -x $script_dir/../bin/interpolation_fix ]]
 then
     echo "Binary available. Proceeding."
 else
@@ -11,15 +11,16 @@ else
     exit 9
 fi
 
-#for fortfile in $(echo fort.*);
 for fortfile in $@
 do
     if [[ -e $(basename $fortfile) ]]
     then
         mkdir -p uninterpolated
         mv $fortfile uninterpolated
-        echo $script_dir/../bin/interpolate_fix uninterpolated/$(basename $fortfile) $fortfile
+        $script_dir/../bin/interpolation_fix uninterpolated/$(basename $fortfile) $fortfile
     else
-        echo $script_dir/../bin/interpolate_fix $fortfile $(basename $fortfile)
+        $script_dir/../bin/interpolation_fix $fortfile $(basename $fortfile)
     fi
+    echo ""
+done
 exit 0
